2f81f24aa14d086e764044f647a7ecb313c989d7
Petru Iulian <petruiulian01@gmail.com>
2023-08-24 12:32:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,22 +11,22 @@ class FirstScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         
-        main_Layout = BoxLayout(orientation='vertical')
-        name_Layout = BoxLayout(orientation='horizontal')
-        age_Layout = BoxLayout(orientation='horizontal')
+        main_Layout = BoxLayout(orientation='vertical', padding=8, spacing=8)
+        name_Layout = BoxLayout(orientation='horizontal',size_hint=(0.8, None), height='30sp')
+        age_Layout = BoxLayout(orientation='horizontal',size_hint=(0.8, None), height='30sp')
 
         instructionText = Label(text = txt_instruction)
-        nameLabel = Label(text = 'Enter your name:')
+        nameLabel = Label(text = 'Enter your name:', halign='right')
         nameInput = TextInput(multiline = False)
         name_Layout.add_widget(nameLabel)
         name_Layout.add_widget(nameInput)
 
-        ageLabel = Label(text = 'Enter your name:')
+        ageLabel = Label(text = 'Enter your name:', halign='right')
         ageInput = TextInput(multiline = False)
         age_Layout.add_widget(ageLabel)
         age_Layout.add_widget(ageInput)
         
-        startBtn = Button(text = 'start')
+        startBtn = Button(text = 'start', size_hint=(0.3, 0.2), pos_hint={'center_x': 0.5})
 
         main_Layout.add_widget(instructionText)
         main_Layout.add_widget(name_Layout)
